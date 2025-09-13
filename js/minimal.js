dab656d8f291493f55e8aa967ead10506c64ab49
Xavier Labourier <xavier.labourier@quarksgroup.com>
2025-09-13 19:54:10 +0200

    Fix v2.2.2: alignement messages envoyés à droite

--- js/minimal.js
+++ js/minimal.js
@@ -1,6 +1,6 @@
 // Configuration de l'application
 const APP_CONFIG = {
-    version: '2.2.1',
+    version: '2.2.2',
     productionUrl: 'https://gzOrg-zOg.github.io/ZogChat/',
     isDevelopment: () => {
         return window.location.hostname === 'localhost' || 
